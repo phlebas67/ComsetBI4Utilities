812ba07c8b151b4c5e81bee0a182aa98a405b7cd
Colin Terry <colin.terry@comset.co.uk>
2024-01-19 11:53:53 +0000

    Modified CreateReportListing so that it utilises a correct Main class, with the ability to accept username, password etc from a command line

--- ComsetBI4Utilities/src/comset/boe4utilities/CreateReportListing.java
+++ ComsetBI4Utilities/src/comset/boe4utilities/CreateReportListing.java
@@ -11,22 +11,91 @@ import com.crystaldecisions.sdk.occa.infostore.IInfoObjects;
 import com.crystaldecisions.sdk.occa.infostore.IInfoStore;
 import com.crystaldecisions.sdk.plugin.desktop.folder.IFolder;
 import com.crystaldecisions.sdk.plugin.desktop.program.IProgramBase;
+import	com.crystaldecisions.sdk.framework.*;
 
 public class CreateReportListing implements IProgramBase{
 	FileWriter fw;
 
 	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) 
+	{
+		// *****************
+		// the main method is only going to be called when the jar is run from the command line. 
+		// If the jar is run a a program file within BI, the entire main method will be skipped.  Keep this in mind when 
+		// adding additional code to the main method.
+		// *****************
+		
+		IEnterpriseSession boEnterpriseSession = null;
+		ISessionMgr boSessionMgr = null;
+		IInfoStore boInfoStore = null;
+		String userName = null;
+		String cmsName = null;
+		String password = null;
+		String authType = null;
+		
+		// Logon Information
+		// To pass the arguments to the main method in the Eclipse IDE, set the values in the configuration. 
+		// (Right Click the .java file > Run As > Run configuration > Arguments tab) 
+		
+		userName = args[0];
+		password = args[1];
+		cmsName = args[2];
+		authType = args[3];
+
+		
+		// Enforce all expected cmd line arguments have been submitted. 
+		// **Remember this is a sample, change values as needed.
+		if ((args.length >= 4 ) && args[0] != null)   
+		{
+			try 
+			{
+				// Initialize the Session Manager 
+				boSessionMgr = CrystalEnterprise.getSessionMgr();
+	
+				// Logon to the Session Manager to create a new BOE session.
+				boEnterpriseSession = boSessionMgr.logon(userName, password, cmsName, authType);
+				System.out.println("user \"" + userName + "\" logged in via main() method");
+				
+				//Retrieve the InfoStore object
+				boInfoStore = (IInfoStore) boEnterpriseSession.getService("", "InfoStore");
+
+				
+			}
+			catch (SDKException sdke)
+			{
+				System.out.println(sdke.getMessage());
+				System.exit(1);
+			}
+			
+			//call the run() method
+			CreateReportListing crl = new CreateReportListing();
+			crl.run(boEnterpriseSession, boInfoStore, args);
+		
+		}  //end of if statement
+	} //end of main method
+
 	public void run(IEnterpriseSession boEnterpriseSession, IInfoStore boInfoStore, java.lang.String[] args) {
 		
 		// Declare variables for the output file
 		
 		int writtenRecords = 0;
 		String resultString;
-		String filePath = args[0];
-		String delimiter = args[1];
+		String filePath="";
+		String delimiter="";
 
-		
+		//Retrieve arguments if run from Command Line
+		if (args.length == 6)
+		{
+				filePath = args[4];
+				delimiter = args[5];
+		}
 			
+		//Retrieve arguments if run from Command Line
+		if (args.length == 2)
+		{
+				filePath = args[0];
+				delimiter = args[1];
+		}
 
 		try {
 			fw = new FileWriter(filePath);
@@ -150,7 +219,7 @@ public class CreateReportListing implements IProgramBase{
 		    {
 			    try {
 					fw.close();
-					System.out.println(writtenRecords + " lines were written to " + args[0]);
+					System.out.println(writtenRecords + " lines were written to " + filePath);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
